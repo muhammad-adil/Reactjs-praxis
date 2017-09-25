dd208bed6005009a683e22d2ad54f001fcb87da9
Muhammad-Adil <adil.muhammad93@gmail.com>
2017-09-25 19:25:50 +0500

    working with events

--- reactjs-basics/src/app/components/Home.js
+++ reactjs-basics/src/app/components/Home.js
@@ -2,19 +2,35 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export class Home extends React.Component {
+  constructor(props) {
+    super();
+    this.age = props.age;
+  }
+
+  onMakeOlder() {
+    this.age += 3;
+    console.log(this.age);
+  }
+
   render() {
     var text = "Something";
-    console.log(this.props);
+    //console.log(this.props);
     return (
       <div>
         <b>{text}</b>
-        <p>I am new Home coponent</p>
-        <p>
+        <p>I am new Home coponent and my age is {this.age}</p>
+        <button
+          onClick={() => this.onMakeOlder()}
+          className="btn btn-primary btn-sm"
+        >
+          Make Me Older
+        </button>
+
+        {/* <p>
           Your Name is {this.props.name} &nbsp; Your age is {this.props.age},
           this Name and Age is coming from props
         </p>
         <i>User Object => Name: {this.props.user.name} </i>
-
         <div>
           <ul>
             {this.props.user.hobbies.map((hobby, i) => (
@@ -22,7 +38,7 @@ export class Home extends React.Component {
             ))}
           </ul>
           {this.props.children}
-        </div>
+        </div> */}
       </div>
     );
   }
