1a11d86931006fa3864eb676bdc2f73fa7aa64dc
Muhammad-Adil <adil.muhammad93@gmail.com>
2017-09-27 16:04:39 +0500

    Movie App, Routing, Initial Work

---
+++ react-routing/src/components/FeaturedMovie.js
@@ -0,0 +1,28 @@
+import React, { PropTypes } from "react";
+
+const FeaturedMovie = ({ movie }) => (
+  <div className="featured-movie">
+    <div className="featured-movie__image">
+      <img alt={movie.name} src={movie.img} />
+    </div>
+
+    <div className="featured-movie__info">
+      <p>
+        <b>{movie.name}</b>
+      </p>
+      <p>{movie.director}</p>
+      <p>{movie.released}</p>
+    </div>
+  </div>
+);
+
+FeaturedMovie.PropTypes = {
+  movie: PropTypes.shape({
+    director: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    released: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired
+  }).isRequired
+};
+
+export default FeaturedMovie;
---
+++ react-routing/src/components/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import movies from "../movies.json";
+import FeaturedMovie from "./FeatureMovie";
+
+const Home = () => {
+  const topFour = movies.slice(0, 4);
+  return (
+    <div>
+      <h2 className="featured-movies__header">Featured Movie</h2>
+      <hr />
+
+      <div className="featured-movies">
+        {topFour.map((movie, i) => <FeaturedMovie movie={movie} key={i} />)}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
