d5242343693c762aaaf89d56969fb0272ed9c6b7
Muhammad-Adil <adil.muhammad93@gmail.com>
2017-09-28 14:49:36 +0500

    specific movie route working

--- react-routing/src/components/PageNotFound.js
+++ react-routing/src/components/PageNotFound.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const PageNotFound = () => (
-  <div className="page-not-found">We're sorry. This page doesn't exist!</div>
+  <div className="page-not-found">
+    We're sorry. This page doesn't exist! error 404
+  </div>
 );
 
 export default PageNotFound;
