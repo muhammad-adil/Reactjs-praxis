f6c58a2014d8f6aea684bd499fce4a08087283a7
Muhammad-Adil <adil.muhammad93@gmail.com>
2019-11-14 20:21:10 +0500

    Just thinking to work with react again

--- react-fetch/src/component/GithubComponent.js
+++ react-fetch/src/component/GithubComponent.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 //import './GithubComponent.css';
 const urlForUsername = username => `https://api.github.com/users/${username}`
 
-
+// eslint-disable
 export default class GithubComponent extends Component {
 
 constructor(props){
@@ -14,6 +14,7 @@ constructor(props){
 }
 
   componentDidMount(){
+    // eslint-disable-next-line
     fetch(urlForUsername(this.props.username))
     .then(response => {
       if (!response.ok ) {
@@ -26,6 +27,7 @@ constructor(props){
       this.setState({
         githubData : d
       })
+      // eslint-disable-next-line
     }),
      () => {
       this.setState({
@@ -42,6 +44,20 @@ constructor(props){
     return (
       <div className="App">
           <h2>{this.state.githubData.name}</h2>
+          <img src="this.state.githubData.avatar_url" alt="avatar"/>
+          {/* <svg width="90" height="90">       
+            <image xlink:href="this.state.githubData.avatar_url" src="" width="90" height="90"/>    
+          </svg> */}
+
+          <h5>Bio: &nbsp;{this.state.githubData.bio}</h5>
+          <date>Created at: &nbsp;{this.state.githubData.created_at}</date>
+          <br/>
+          <number>Followers: &nbsp;{this.state.githubData.followers}</number>
+          <br/>
+          <address>Location: &nbsp;{this.state.githubData.location}</address>
+          <br/>
+          <url>Url: &nbsp;{this.state.githubData.url}</url>
+
         <p className="App-intro"></p>
       </div>
     );
