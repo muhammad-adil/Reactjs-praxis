d818023b15ab6aad06685ea2d97b98fd53b8f52f
Muhammad-Adil <adil.muhammad93@gmail.com>
2020-03-21 00:34:46 +0100

    React todo form

--- 02-react-todo-app/src/App.js
+++ 02-react-todo-app/src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  useState();
+  const [todos, setTodos] =  useState([]);
   return (
     <div className="App">
       <header className="App-header">
