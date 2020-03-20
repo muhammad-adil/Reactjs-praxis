db5ff50ed3ad3656940e70680c37459e74bb528e
Muhammad-Adil <adil.muhammad93@gmail.com>
2020-03-21 00:29:28 +0100

    React todo start

---
+++ 02-react-todo-app/src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  useState();
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>React Todo App</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
