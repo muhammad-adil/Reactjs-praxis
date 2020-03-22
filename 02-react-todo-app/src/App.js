985edc2857ecf98f886e8db8c322c524536f4a6e
Muhammad-Adil <adil.muhammad93@gmail.com>
2020-03-22 02:09:01 +0100

    Handling events

--- 02-react-todo-app/src/App.js
+++ 02-react-todo-app/src/App.js
@@ -1,14 +1,21 @@
 import React, { useState } from 'react'; // useState is a function, takes initial state as a property
 import logo from './logo.svg';
 import './App.css';
+import TodoForm from "./components/TodoForm";
 
 function App() {
   const [todos, setTodos] =  useState([]); // first item is state itself, 2nd is function used to update state, because todo state is immutable.
+  
+  function addTodo(todo){
+    setTodos([todo, ...todos]);
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>React Todo App</h2>
+        <TodoForm addTodo={addTodo} />
       </header>
     </div>
   );
--- 02-react-todo-app/src/components/TodoForm.js
+++ 02-react-todo-app/src/components/TodoForm.js
@@ -1,16 +1,34 @@
 import React, { useState } from "react";
+import uuid from "uuid";
 
-function TodoForm(){
+function TodoForm({ addTodo }){
     useState();
     const [todo, setTodo] = useState({
         id: "",
         task: "",
         completed: false
     });
+
+    // for updating the task property
+    function handleTaskInputChange(e){
+      setTodo({ ...todo, task: e.target.value })
+    }
+
+    function handleSubmit(e){
+      e.preventDefault();
+        if(todo.task.trim()){
+            addTodo({ ...todo, id: uuid.v4() })
+        }
+    }
+
     return(
         <form>
-            <input/>
-            <button/>
+            <input
+                name="task" 
+                type="text" 
+                value={todo.task} 
+                onChange={ handleTaskInputChange }/>
+            <button type="submit" />
         </form>
     )
 }
