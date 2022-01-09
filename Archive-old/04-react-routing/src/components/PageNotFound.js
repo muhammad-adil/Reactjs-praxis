7d973f295946de57e896444fa7962c40cdcf243f
Muhammad-Adil <adil.muhammad93@gmail.com>
2022-01-09 15:40:50 +0100

    Repository cleanup

--- Archive-old/04-react-routing/src/components/PageNotFound.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const PageNotFound = () => (
-  <div className="page-not-found">
-    We're sorry. This page doesn't exist! error 404
-  </div>
-);
-
-export default PageNotFound;
--- Archive-old/old-todo-app/src/components/Header.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-
-export default class Header extends Component {
-  // super(props);
-  render() {
-    return <h2 className="App-intro">Hello from header.js in component</h2>;
-  }
-}
