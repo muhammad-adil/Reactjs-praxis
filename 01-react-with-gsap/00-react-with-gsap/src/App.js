5f40f58fd456b49fbe00dc8984d6dd5ec2662efd
Muhammad-Adil <adil.muhammad93@gmail.com>
2022-04-20 14:15:30 +0200

    ease and deay dded to the animation

--- 01-react-with-gsap/00-react-with-gsap/src/App.js
+++ 01-react-with-gsap/00-react-with-gsap/src/App.js
@@ -7,21 +7,36 @@ import { gsap, Power3 } from "gsap";
 function App() {
   let logoItem = useRef(null);
   let textItem = useRef(null);
+  let linkItem = useRef(null);
 
-  console.log(logoItem, "logoItem");
+  // console.log(logoItem, "logoItem");
 
   useEffect(() => {
-    console.log(logoItem);
-    gsap.to(logoItem, 0.8, { opacity: 1, y: -20, ease: Power3.easeOut });
-    gsap.to(textItem, 0.8, {
+    // console.log(logoItem);
+    gsap.to(logoItem, {
       opacity: 1,
-      y: -20,
+      duration: 3,
+      y: -50,
       ease: Power3.easeOut,
+      rotation: 111
+    });
+    gsap.to(textItem, {
+      opacity: 1,
+      duration: 1,
+      y: -25,
+      ease: Power3.easeInOut,
       delay: 0.2
+    });
+    gsap.to(linkItem, {
+      opacity: 1,
+      duration: 1.2,
+      y: -15,
+      ease: Power3.easeout,
+      delay: 0.3
     });
   }, []);
 
-  console.log(logoItem, "logoItem");
+  // console.log(logoItem, "logoItem");
 
   return (
     <div className="App">
@@ -38,10 +53,14 @@ function App() {
           ref={(el) => {
             textItem = el;
           }}
+          className="caption"
         >
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
+          ref={(el) => {
+            linkItem = el;
+          }}
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
