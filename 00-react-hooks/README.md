fff87d1ac0e2a7119c9f46ba3913e45b4edf8197
Muhammad-Adil <adil.muhammad93@gmail.com>
2023-10-26 00:37:37 +0200

    useState Hooks with previous state

--- 00-react-hooks/README.md
+++ 00-react-hooks/README.md
@@ -1,3 +1,4 @@
 # React Hooks App
 
+Only Call Hooks from React Funcitons. Call them from within react functional components and not just any regular JS function.
 Don't call Hooks inside loops, conditions or nested functions.
--- 00-react-hooks/src/components/01-use-state.tsx
+++ 00-react-hooks/src/components/01-use-state.tsx
@@ -2,12 +2,40 @@ import React, { useState } from "react";
 import "./../App.css";
 
 function UseStateHook() {
+  const initialCount = 0;
+  //
   const [count, setCount] = useState(0);
+  const [count01, setCount01] = useState(initialCount);
+  //
+  const IncrementFive = () => {
+    for (let i = 0; i < 5; i++) {
+      setCount01((prevCount) => prevCount + 1);
+    }
+  };
   return (
-    <div className="card">
-      <h2>React UseState</h2>
-      <button onClick={() => setCount(count + 1)}>Count {count}</button>
-    </div>
+    <>
+      <div className="card">
+        <h4>React UseState</h4>
+        <i>{count}</i>
+        <br />
+        <button onClick={() => setCount(count + 1)}>Count&nbsp;{count}</button>
+      </div>
+      <div className="card">
+        <h4>React UseState with previous State</h4>
+        {/*  */}
+        <i>Count: {count01}</i>
+        <br />
+        <button onClick={() => setCount01(initialCount)}>Reset </button>
+        <button onClick={() => setCount01((prevCount) => prevCount + 1)}>
+          Increment
+        </button>
+        <button onClick={() => setCount01((prevCount) => prevCount - 1)}>
+          Decrement
+        </button>
+        <button onClick={IncrementFive}>IncrementFive</button>
+        {/*  */}
+      </div>
+    </>
   );
 }
 
