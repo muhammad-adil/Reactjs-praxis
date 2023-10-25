444bc82029c7356e479cd7db74eafc1f2d2e9e01
Muhammad-Adil <adil.muhammad93@gmail.com>
2023-10-25 17:10:25 +0200

    React useState Hook

--- 00-react-hooks/src/App.tsx
+++ 00-react-hooks/src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+//
+import UseStateHook from "./components/01-use-state";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">React HOOKS 101</header>
+      <div className="container">
+        <div className="content">
+          <UseStateHook />
+        </div>
+      </div>
     </div>
   );
 }
