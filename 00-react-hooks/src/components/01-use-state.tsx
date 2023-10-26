0409741ca274498527a43a35e5188ae0b1d396b4
Muhammad-Adil <adil.muhammad93@gmail.com>
2023-10-26 15:30:00 +0200

    useState with Object

--- 00-react-hooks/src/components/01-use-state.tsx
+++ 00-react-hooks/src/components/01-use-state.tsx
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
 import "./../App.css";
 
+interface IUser {
+  firstName: string;
+  lastName: string;
+}
+
 function UseStateHook() {
   const initialCount = 0;
   //
   const [count, setCount] = useState(0);
   const [count01, setCount01] = useState(initialCount);
+
+  // useState with Object
+  const [name, setName] = useState<IUser>({ firstName: "", lastName: "" });
+
   //
   const IncrementFive = () => {
     for (let i = 0; i < 5; i++) {
@@ -33,7 +42,48 @@ function UseStateHook() {
           Decrement
         </button>
         <button onClick={IncrementFive}>IncrementFive</button>
-        {/*  */}
+      </div>
+      {/*  */}
+      {/* useState with Object */}
+      <div className="card">
+        <h4>useState with Object</h4>
+        <form>
+          <input
+            type="text"
+            value={name.firstName}
+            onChange={(e) => setName({ ...name, firstName: e.target.value })}
+          />
+          <input
+            type="text"
+            value={name.lastName}
+            onChange={(e) => setName({ ...name, lastName: e.target.value })}
+          />
+          <h4>Your first Nmae is {name.firstName}</h4>
+          <h4>Your last Nmae is {name.lastName}</h4>
+          {/*  */}
+          <h6>{JSON.stringify(name)}</h6>
+        </form>
+      </div>
+
+      {/*  */}
+      {/* useState with Array */}
+      <div className="card">
+        <h4>useState with Array</h4>
+        {/* <form>
+          <input
+            type="text"
+            value={name.firstName}
+            onChange={(e) => setName({ ...name, firstName: e.target.value })}
+          />
+          <input
+            type="text"
+            value={name.lastName}
+            onChange={(e) => setName({ ...name, lastName: e.target.value })}
+          />
+          <h4>Your first Nmae is {name.firstName}</h4>
+          <h4>Your last Nmae is {name.lastName}</h4>
+          <h6>{JSON.stringify(name)}</h6>
+        </form> */}
       </div>
     </>
   );
