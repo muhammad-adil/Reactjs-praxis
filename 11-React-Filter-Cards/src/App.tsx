ea6aa39fa6f716ea7feb5475101f52b73967b17c
muhammad-adil <adil.muhammad93@gmail.com>
2024-01-05 15:30:00 +0100

    React movie filter app complete with animaion

--- 11-React-Filter-Cards/src/App.tsx
+++ 11-React-Filter-Cards/src/App.tsx
@@ -1,16 +1,22 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+//  Framer Motion
+import { motion, AnimatePresence } from 'framer-motion'
+// Components
 import Movie from './components/Movie'
-import Filter from './components/Filter'
+import FilterMovies from './components/FilterMovies'
 
 function App() {
   const [popular, setPopular] = useState([])
+  const [filtered, setFiltered] = useState([])
+  const [activeGenre, setActiveGenre] = useState(0)
 
   const fetchPpular = async () => {
     const response = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=7716ce1f9f2e8811557dd7a292cb4cde&language=en-US&page=1')
     const movies = await response.json()
     console.log(movies)
     setPopular(movies.results)
+    setFiltered(movies.results)
   }
   
   useEffect(() => {
@@ -20,13 +26,14 @@ function App() {
   return (
     <>
       <div className='App'>
-        <Filter/>
-        <div className='popular-movies'>
-          {popular.map((movie: any) => (
+        <FilterMovies popular={popular} setFiltered={setFiltered} activeGenre={activeGenre} setActiveGenre={setActiveGenre} />
+        <motion.div layout className='popular-movies'>
+          <AnimatePresence>
+          {filtered.map((movie: any) => (
             <Movie key={movie.id} movie={movie} />
           ))}
-          {/* <Movie /> */}
-        </div>
+          </AnimatePresence>
+        </motion.div>
       </div>
     </>
   )
--- 11-React-Filter-Cards/src/components/Movie.tsx
+++ 11-React-Filter-Cards/src/components/Movie.tsx
@@ -1,22 +1,32 @@
-import React from 'react'
+import React from "react";
+//  Framer Motion
+import { motion } from "framer-motion";
 
 interface Movie {
-    title:string;
-    overview:string;
-    poster_path:string;
-    release_date:string;
+  title: string;
+  overview: string;
+  poster_path: string;
+  release_date: string;
 }
 
-function Movie({movie: Movie}: any) {
-
-    return (
-        <div className='popular-movie'>
-            {/* <h2>Movie</h2> */}
-            <h2>{Movie.title}</h2>
-            <img src={`https://image.tmdb.org/t/p/w500${Movie.backdrop_path}`} alt={Movie.title} />
-            {/* <p>{Movie.overview}</p> */}
-        </div>
-    )
+function Movie({ movie: Movie }: any) {
+  return (
+    <motion.div
+      layout
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      className="popular-movie"
+    >
+      {/* <h2>Movie</h2> */}
+      <h2>{Movie.title}</h2>
+      <img
+        src={`https://image.tmdb.org/t/p/w500${Movie.backdrop_path}`}
+        alt={Movie.title}
+      />
+      {/* <p>{Movie.overview}</p> */}
+    </motion.div>
+  );
 }
 
-export default Movie
+export default Movie;
